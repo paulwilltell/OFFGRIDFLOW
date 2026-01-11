9073dac047a7ca8dce9234a6a06f088ee17f479e
Paul Timchuk <paultimchuk@gmail.com>
2026-01-10 16:09:01 -0800

    Remove standalone mode, keep typescript ignore

--- web/next.config.js
+++ web/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Force all pages to be dynamic (no static prerendering)
-  output: 'standalone',
-  
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
